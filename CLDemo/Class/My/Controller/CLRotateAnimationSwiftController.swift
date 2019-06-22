c460843ff173fdbc3dce779f7760e07b4bb828ca
青锋 <>
2019-06-22 19:08:37 +0800

    fix

--- CLDemo/Class/My/Controller/CLRotateAnimationSwiftController.swift
+++ CLDemo/Class/My/Controller/CLRotateAnimationSwiftController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class CLRotateAnimationSwiftController: CLBaseViewController {
 
+    let timer = CLGCDTimer.init(interval: 0.1, queue: DispatchQueue.main, action: nil)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         do {
-            let rotateAnimationView: CLRotateAnimationView = CLRotateAnimationView(frame: CGRect(x: 120, y: 120, width: 80, height: 80));
+            let rotateAnimationView: CLRotateAnimationView = CLRotateAnimationView(frame: CGRect(x: 80, y: 120, width: 80, height: 80));
             rotateAnimationView.updateWithConfigure { (configure) -> (Void) in
                 configure.backgroundColor = UIColor.orange;
                 configure.number = 8;
@@ -26,7 +28,7 @@ class CLRotateAnimationSwiftController: CLBaseViewController {
         }
 
         do {
-            let roundAnimationView = CLRoundAnimationView(frame: CGRect(x: 120, y: 250, width: 90, height: 90))
+            let roundAnimationView = CLRoundAnimationView(frame: CGRect(x: 90, y: 250, width: 90, height: 90))
             roundAnimationView.updateWithConfigure { (configure) -> (Void) in
                 configure.outBackgroundColor = UIColor(red:1.00, green:0.00, blue:0.01, alpha:0.60)
                 configure.inBackgroundColor = UIColor(red:0.28, green:0.54, blue:0.96, alpha:1.00)
@@ -56,5 +58,23 @@ class CLRotateAnimationSwiftController: CLBaseViewController {
             view.addSubview(roundAnimationView)
             roundAnimationView.startAnimation()
         }
+        
+        do {
+            let sectorAnimationView = CLSectorAnimationView(frame: CGRect(x: 200, y: 120, width: 80, height: 80))
+            view.addSubview(sectorAnimationView)
+            timer.replaceOldAction { actions in
+                let progress = CGFloat(CGFloat(actions) / 100.0)
+                sectorAnimationView.updateProgress(progress: progress)
+            }
+            timer.start()
+        }
+        
+        
+        do {
+            let sectorAnimationView = CLSectorAnimationView(frame: CGRect(x: 220, y: 220, width: 80, height: 80))
+            view.addSubview(sectorAnimationView)
+            sectorAnimationView.updateProgressAnimation(fromValue: 0.5, toValue: 1, duration: 6)
+        }
+        
     }
 }
